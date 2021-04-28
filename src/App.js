bd2534f6748d5175b13e4081da359c604dfdf0b1
Thomas <dosanjos.thomas@gmail.com>
2021-04-28 10:59:07 +0200

    add render on currentUser, add const user.types

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -41,14 +41,28 @@ class App extends React.Component {
         <Switch>
           <Route component={HomePage} exact path="/" />
           <Route component={ShopPage} path="/shop" />
-          <Route component={SignInAndSignUpPage} path="/signin" />
+          <Route
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+            exact
+            path="/signin"
+          />
         </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
