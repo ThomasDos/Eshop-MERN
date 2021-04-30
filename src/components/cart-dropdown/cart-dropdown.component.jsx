53b85bba12fddb685b34b5ccf1ac1540efc34f2b
Thomas <dosanjos.thomas@gmail.com>
2021-04-30 11:05:37 +0200

    add reselect lib, change component dependence on the store by selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import CustomButton from "./../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 import CartItem from "./../cart-item/cart-item.component";
 import { connect } from "react-redux";
+import { selectCartItems } from "./../../redux/cart/cart.selectors";
 
 const CartDropdown = ({ cartItems }) => {
   return (
@@ -18,8 +19,8 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
