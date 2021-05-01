13b37efbf5d3a4084da6a084786743d743564824
Thomas <dosanjos.thomas@gmail.com>
2021-05-01 13:00:44 +0200

    update file name Collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CollectionsOverview from "./../../components/collections-overview/collections-overview.component";
 
 import { Route } from "react-router-dom";
-import CollectionPage from "../collection/collection.component";
+import CollectionPage from "../Collection/collection.component";
 
 const ShopPage = ({ match }) => (
   <div className="shop-page">
