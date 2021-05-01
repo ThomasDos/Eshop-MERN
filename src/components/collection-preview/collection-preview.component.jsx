a8553ae19c26e219f23cfe0669096f5db7402327
Thomas <dosanjos.thomas@gmail.com>
2021-05-01 13:06:48 +0200

    update link onclick title collection-preview comp

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import "./collection-preview.styles.scss";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      {/* <Link to={`/shop/` + title.toLowerCase()}> */}
-      <h1 className="title">{title.toUpperCase()}</h1>
-      {/* </Link> */}
+      <Link to={`/shop/` + title.toLowerCase()}>
+        <h1 className="title">{title.toUpperCase()}</h1>
+      </Link>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
