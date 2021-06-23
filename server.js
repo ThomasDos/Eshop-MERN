99d3d79005b8b8e5d8f9eaa9824c0b3fa8dbebc3
Thomas <dosanjos.thomas@gmail.com>
2021-06-23 19:08:06 +0200

    update manifest and favicons

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  // app.use(enforce.HTTPS({trustProtoHeader: true}))
+  app.use(express.static(path.join(__dirname, "client / build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
