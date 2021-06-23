2ab7ffd39303c17af4cab33c8081e428f685cd5b
Thomas <dosanjos.thomas@gmail.com>
2021-06-23 20:18:23 +0200

    test heroku

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(enforce.HTTPS({trustProtoHeader: true}))
-  app.use(express.static(path.join(__dirname, "client / build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
